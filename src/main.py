666b24b5207d04f9f9d0f5f4b12af038e3631535
Alisam-alkhalil <alisamalkhalil99@gmail.com>
2025-01-15 16:24:52 +0000

    Fixed main andd requirements file

--- src/main.py
+++ src/main.py
@@ -1,14 +1,6 @@
 import os
-import requests
-import json
-import pandas as pd
-import time
-import matplotlib.pyplot as plt
-import streamlit as st
-import csv
 from dotenv import load_dotenv
-from datetime import datetime
-import extract, transform, load, website
+import extract, transform, load
 
 load_dotenv()
 
@@ -16,20 +8,13 @@ api_key = os.getenv("API_KEY")
 url = os.getenv("URL")
 
 
-products_list = extract.get_data(url, api_key)
-products_list = transform.get_profit_data(products_list)
-sorted_products = sorted(products_list,key = lambda x: x["true_hourly_profit"], reverse=True)
+if __name__ == "__main__":
 
-load.load_data(sorted_products[:500])
-
-
-
-
-# print(json.dumps(sorted_products[0:10], indent=4))
+    products_list = extract.get_data(url, api_key)
+    products_list = transform.get_profit_data(products_list)
+    sorted_products = sorted(products_list,key = lambda x: x["true_hourly_profit"], reverse=True)
+    load.load_data(sorted_products[:500])
 
 
-
-
-website.create_webpage("../data.csv", sorted_products[:500])
 
 
